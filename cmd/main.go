35b72651cbdda63189f5becf8e310607d6fe0921
Kashif Minhaj <dailypctips@gmail.com>
2020-09-02 17:01:23 +0530

    Move template to text file, add more mongodb functions

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"text/template"
@@ -23,7 +24,12 @@ func main() {
 	flag.StringVar(&d.Name, "name", "User", "The prefix name for model structs")
 	flag.Parse()
 
-	t := template.Must(template.New("gogoose").Parse(queueTemplate))
+	templateBytes, err := ioutil.ReadFile("templates/main.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	t := template.Must(template.New("gogoose").Parse(string(templateBytes)))
 	rc, wc, _ := pipe.Commands(
 		exec.Command("gofmt"),
 		exec.Command("goimports"),
@@ -32,94 +38,3 @@ func main() {
 	wc.Close()
 	io.Copy(os.Stdout, rc)
 }
-
-var queueTemplate = `
-package gen
-
-import (
-	"context"
-	"errors"
-	"reflect"
-
-	"{{.StructImport}}"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
-
-type {{.Name}}Model struct {
-	dbColl *mongo.Collection
-}
-
-type {{.Name}}Document struct {
-	dbColl *mongo.Collection
-	raw    *{{.StructType}}
-}
-
-func (userDocument *{{.Name}}Document) Save(ctx context.Context) error {
-	// TODO: implement difftracker
-	if userDocument.raw.ID == nil {
-		return errors.New("_id is nil")
-	}
-	structValueRef := reflect.ValueOf(userDocument.raw).Elem()
-	structTypeRef := reflect.TypeOf(userDocument.raw).Elem()
-
-	nFields := structTypeRef.NumField()
-	fieldToUpdate := bson.M{}
-	for i := 0; i < nFields; i++ {
-		field := structTypeRef.Field(i)
-		fieldName := gogoose.GetBsonName(field)
-		if fieldName == "_id" {
-			continue
-		}
-		// fmt.Println(structValueRef.Field(i))
-		fieldToUpdate[fieldName] = structValueRef.Field(i).Interface()
-	}
-	_, err := userDocument.dbColl.UpdateOne(ctx, bson.M{"_id": userDocument.raw.ID}, bson.M{"$set": fieldToUpdate}, options.Update().SetUpsert(true))
-	return err
-}
-
-func (userModel *{{.Name}}Model) Find(ctx context.Context, filter interface{}, opts ...*options.FindOptions) ([]*{{.Name}}Document, error) {
-	cursor, err := userModel.dbColl.Find(ctx, filter, opts...)
-	if err != nil {
-		return nil, err
-	}
-	docs := make([]*{{.Name}}Document, 0, 0)
-	for cursor.Next(ctx) {
-		user := &{{.StructType}}{}
-		err := bson.Unmarshal(cursor.Current, user)
-		if err != nil {
-			return docs, err
-		}
-	}
-	return docs, cursor.Err()
-}
-
-func (userModel *{{.Name}}Model) FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) (*{{.Name}}Document, error) {
-	res := userModel.dbColl.FindOne(ctx, filter)
-	user := &{{.StructType}}{}
-	err := gogoose.DecodeSingleResult(res, user)
-	if err != nil {
-		return nil, err
-	}
-	return New{{.Name}}Document(user, userModel.dbColl), nil
-}
-
-func (userModel *{{.Name}}Model) New(user *{{.StructType}}) *{{.Name}}Document {
-	return &{{.Name}}Document{raw: user, dbColl: userModel.dbColl}
-}
-
-// New{{.Name}}Document ...
-func New{{.Name}}Document(user *{{.StructType}}, coll *mongo.Collection) *{{.Name}}Document {
-	return &{{.Name}}Document{
-		dbColl: coll,
-		raw:    user,
-	}
-}
-
-func New{{.Name}}Model(collection *mongo.Collection) *{{.Name}}Model {
-	return &{{.Name}}Model{dbColl: collection}
-}
-
-`
